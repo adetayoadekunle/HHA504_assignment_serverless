9f7e0837697d84fc4ce61ccfb1ee7b1d57a206c0
Adetayo Adekunle <adetayo.adekunle@stonybrook.edu>
2024-11-24 23:30:49 -0500

    nc

--- send_email.py
+++ send_email.py
@@ -1,13 +1,18 @@
 import os
 import resend 
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
 resend.api_key = os.environ["RESEND_API_KEY"]
 
 params: resend.Emails.SendParams = {
-    "from": "adetayo",
+    "from": "adetayo.adekunle@stonybrook.,edu",
     "to": ["adetayo.adekunle@stonybrook.edu"],
     "subject": "Hello, Adetayo Adekunle, Schedule task executed",
     "html": "<strong>it works!</strong>",
 }
 
-email = "resend.Emails.send(params)"
+email = resend.Emails.send(params)
 print(email)
